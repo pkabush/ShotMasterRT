03a035a502872114b3528d5aab5a54c60770f2fd
kabushPasha <kabushpavel@gmail.com>
2026-01-15 16:13:16 +0100

    update test

--- src/classes/Shot.ts
+++ src/classes/Shot.ts
@@ -184,7 +184,7 @@ export class Shot {
         } ); 
 
         const task = this.addTask(task_info.id, { provider: ai_providers.KLING ,workflow:task_info.workflow})
-        //await new Promise(res => setTimeout(res, 2000));
+        await new Promise(res => setTimeout(res, 2000));
         console.log("created_task");
         await task.check_status();
       }
